73d8b62b6a28b879beaa3388031ea86442783341
Daniel Bengl <daniel.bengl@renuo.ch>
2025-12-07 20:16:38 +0100

    Add super duper cool rails config

--- nvim/lua/plugins/rails.lua
+++ nvim/lua/plugins/rails.lua
@@ -1,6 +1,69 @@
-return {}
--- return {
---   "tpope/vim-rails",
---   lazy = false,
---   ft = { "ruby" },
--- }
+return {
+  {
+    "RRethy/nvim-treesitter-endwise",
+    lazy = false
+  },
+  {
+    "tpope/vim-rails",
+    lazy = false,
+    ft = { "ruby", "eruby" },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruby_lsp = {
+          cmd = { "mise", "exec", "--", "ruby-lsp" },
+          init_options = {
+            enabledFeatures = {
+              "codeActions",
+              "codeLens",
+              "completion",
+              "definition",
+              "diagnostics",
+              "documentHighlights",
+              "documentLink",
+              "documentSymbols",
+              "foldingRanges",
+              "formatting",
+              "hover",
+              "inlayHint",
+              "onTypeFormatting",
+              "selectionRanges",
+              "semanticHighlighting",
+              "signatureHelp",
+              "typeHierarchy",
+              "workspaceSymbol",
+            },
+            addonSettings = {
+              ["Ruby LSP Rails"] = {
+                enablePendingMigrationsPrompt = false,
+              },
+            },
+          },
+        },
+        solargraph = {
+          commandPath = "/Users/dani/.local/share/mise/installs/ruby/3.4.7/bin/solargraph",
+          -- cmd = { "mise", "exec", "--", "solargraph", "stdio" },
+        },
+        rubocop = { cmd = { "mise", "exec", "--", "rubocop", "--lsp" } },
+        standardrb = { cmd = { "mise", "exec", "--", "standardrb", "--lsp" } },
+        herb_ls = { cmd = { "mise", "exec", "--", "herb-language-server", "--stdio" } },
+        stimulus_ls = {
+          cmd = { "mise", "exec", "--", "stimulus-language-server", "--stdio" },
+          filetypes = { "html", "ruby", "eruby", "blade", "php" },
+        },
+        stylelint_lsp = {
+          cmd = { "mise", "exec", "--", "stylelint-lsp", "--stdio" },
+          filetypes = { "css", "scss", "less", "sass", "vue", "html" },
+          settings = {
+            stylelintplus = {
+              autoFixOnSave = true,
+              autoFixOnFormat = true,
+            },
+          },
+        },
+      },
+    },
+  },
+}
